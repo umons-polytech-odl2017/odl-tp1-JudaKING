f54040a125f99016fab125736209048260de8f5a
JudaKING <Julien.DALNE@student.umons.ac.be>
2017-10-13 16:24:08 +0200

    exo 1 fini

--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,25 @@
 package exercise1;
 
 class Person {
+
+	private String name;
+	private int age;
+
+
+
+	public String getName(){
+			return(name);
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+
+
 }
